d756e955950600fb4a30e58fa3a3bc0c9bb209a3
kabikira <kabikirar2@protonmail.com>
2023-03-12 22:03:16 +0900

    コメント修正

--- ScrollViewCode/SceneDelegate.swift
+++ ScrollViewCode/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        //UIwindwosのアンラップ
+        //UIWindowのアンラップ
         guard let secene = (scene as? UIWindowScene) else { return }
         // window生成
         let window = UIWindow(windowScene: secene)
